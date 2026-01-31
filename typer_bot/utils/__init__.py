9bbbef80beb931e66354a2f07b5dfe989655e30d
adrunkhuman <16039109+adrunkhuman@users.noreply.github.com>
2026-01-31 20:59:40 +0100

    fix: export parse_line_predictions in utils package

--- typer_bot/utils/__init__.py
+++ typer_bot/utils/__init__.py
@@ -1,6 +1,11 @@
 """Utility functions and helpers."""
 
-from .prediction_parser import format_standings, parse_predictions
+from .prediction_parser import format_standings, parse_line_predictions, parse_predictions
 from .scoring import calculate_points
 
-__all__ = ["parse_predictions", "format_standings", "calculate_points"]
+__all__ = [
+    "parse_predictions",
+    "parse_line_predictions",
+    "format_standings",
+    "calculate_points",
+]
